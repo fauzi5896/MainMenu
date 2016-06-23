5b7a0ff73abe48b81eedf78bbb3cbb2ff9470e8a
fauzi <fauzialam5@gmail.com>
2016-06-23 22:27:25 +0700

    Main Menu

--- app/src/main/java/com/example/fauzi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/fauzi/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.fauzi.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final LinearLayout button = (LinearLayout) findViewById(R.id.btnVisit);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
     }
-}
+    }
+
+
+
+
